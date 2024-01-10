0f7b86b1b99a58d4ede1c9fc36d3b5daa5deabb0
castillojohan <castillojohan@orange.fr>
2024-01-10 11:58:50 +0100

    added favorite theme color to localstorage, completed missing part of html build

--- integration/js/theme/theme.js
+++ integration/js/theme/theme.js
@@ -3,26 +3,43 @@ const theme = {
     pathToBody : document.querySelector("body"),
 
     init : () => {
-       theme.bindButton(); 
+        theme.bindButtons();
+        theme.loadFavoriteTheme();
     },
 
-    bindButton : () => {
+    bindButtons : () => {
         const buttons = document.querySelectorAll(".theme-button");
         buttons.forEach((button) => {
-            document.addEventListener("click", theme.handleChangeColor);
+            button.addEventListener("click", theme.handleChangeColor);
         });
     },
 
     handleChangeColor : (event) => {
         const pickedColor = event.target.ariaLabel ;
-        console.log(pickedColor);
         theme.changeToPickedColor(pickedColor);
     },
 
     changeToPickedColor : (newColor)=>{
         theme.pathToBody.classList.remove('theme-green', 'theme-red', 'theme-blue');
         theme.pathToBody.classList.add(newColor);
-    } 
+
+        theme.toLocalStorage('theme', newColor);
+    },
+
+    toLocalStorage : (key, value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+
+    loadFromLocalStorage : (key) => {
+        return JSON.parse(localStorage.getItem(key));
+    },
+
+    loadFavoriteTheme : () => {
+        const favoriteColorTheme = theme.loadFromLocalStorage('theme');
+        if (favoriteColorTheme){
+            theme.changeToPickedColor(favoriteColorTheme);
+        }
+    }
 }
 
 export default theme;
