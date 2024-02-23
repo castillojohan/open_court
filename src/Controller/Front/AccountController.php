ab2b39755b4db8458763076cd66966dc8d768240
castillojohan <castillojohan@orange.fr>
2024-02-23 18:43:58 +0100

    member can be modified, possess gender/age/pincode, conditional display to protect minors members, wip:pin auth for adults members

--- src/Controller/Front/AccountController.php
+++ src/Controller/Front/AccountController.php
@@ -4,17 +4,14 @@ namespace App\Controller\Front;
 
 use App\Entity\Member;
 use App\Entity\User;
-use App\Form\MemberType;
 use App\Repository\MemberRepository;
 use App\Repository\UserRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -79,56 +76,13 @@ class AccountController extends AbstractController
         $currentUser = $userRepository->find($userId);
         $userMail = $currentUser->getEmail();
         $memberCollection = $memberRepository->findBy(['user'=>$userId]);
+        foreach ($memberCollection as $member) {
+            $member->getAge();
+        }
         
         $this->addFlash('success', "Bienvenue, $userMail");
 
-        return $this->render('Front/account.html.twig', ['members'=> $memberCollection, 'session'=>$request->getSession()]);
-    }
-
-    #[Route('/member-choice/{memberId}', name: 'app_member_choice', methods:'GET')]
-    public function memberChoice(Member $memberId, Request $request): Response
-    {
-        $session = $request->getSession();
-        $session->set('member', $memberId);
-        return $this->redirectToRoute('app_account');
-    }
-
-
-    #[Route('/register-member', name: 'app_register_member', methods:['GET', 'POST'])]
-    public function memberRegistration(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $member = new Member();
-        $member->setUser($this->getUser());
-        $form = $this->createForm(MemberType::class, $member);
-        
-        if($request->isMethod('POST')){
-
-            $submittedToken = $request->getPayload()->get('_token');
-            if($this->isCsrfTokenValid('member', $submittedToken)){
-                $form->submit($request->getPayload()->all());
-
-                if($form->isSubmitted() && $form->isValid()){
-                    $entityManager->persist($member);
-                    $entityManager->flush();
-                }
-                return $this->redirectToRoute('app_account');
-            }
-            $this->addFlash('error', 'Oups something went wrong');
-            return $this->render('Front/register-member.html.twig');
-        }
-        return $this->render('Front/register-member.html.twig');
-    }
-
-    #[Route('/member/delete/{memberId}', name: 'app_member_delete')]
-    public function memberDeletion(Member $memberId, EntityManagerInterface $entityManager): Response
-    {
-        if(!$memberId){
-            return new Exception('Not Found', Response::HTTP_NOT_FOUND);
-        }
-        
-        $entityManager->remove($memberId);
-        $entityManager->flush();
-        return $this->redirectToRoute('app_account');
+        return $this->render('Front/account.html.twig', ['members'=> $memberCollection, 'session'=>$request->getSession(), "dateNow" => new DateTimeImmutable()]);
     }
 
     /**
--- src/Form/MemberType.php
+++ src/Form/MemberType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Member;
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,9 +17,33 @@ class MemberType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName', TextType::class)
-            ->add('lastName', TextType::class)
-            ->add('age', DateType::class)
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom de famille'
+            ])
+            ->add('birthday', DateType::class, [
+                'label' => 'Date de naissance'
+            ])
+            ->add('gender', ChoiceType::class, [
+                'label' => "Genre",
+                'choices' => [
+                    'Homme' => false,
+                    'Femme' => true
+                ]
+            ])
+            ->add('pincode', PasswordType::class, [
+                "attr" => [
+                    "placeholder" => "Laisser vide si pas de modification du code pin"
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                "label" => "Enregistrer",
+                "attr" => [
+                    "class" => "button",
+                ]
+            ])
         ;
     }
 
@@ -27,7 +53,10 @@ class MemberType extends AbstractType
             'data_class' => Member::class,
             'csrf_protection' => true,
             'csrf_field_name' => '_token',
-            'csfr_token_id' => 'member'
+            'csfr_token_id' => 'member',
+            'attr' => [
+                'novalidate' => 'novalidate'
+            ]
         ]);
     }
 }
